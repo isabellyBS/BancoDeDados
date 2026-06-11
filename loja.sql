c06314c73a2c8f5b81ceed007600a7b20bd967af
isabellyBS <isabellyBSmcm@gmail.com>
2026-06-11 12:24:22 -0300

    Add files via upload

--- loja.sql
+++ loja.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 21-Maio-2026 às 17:25
--- Versão do servidor: 10.4.22-MariaDB
--- versão do PHP: 8.1.2
+-- Tempo de geração: 11-Jun-2026 às 17:23
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.0.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,7 +32,19 @@ USE `loja`;
 CREATE TABLE `categorias` (
   `id_categoria` int(11) NOT NULL,
   `categoria` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `categorias`
+--
+
+INSERT INTO `categorias` (`id_categoria`, `categoria`) VALUES
+(1, 'Eletrônicos'),
+(2, 'Roupas'),
+(3, 'Enlatados'),
+(4, 'Limpeza'),
+(5, 'Hortifuit'),
+(6, 'Açougue');
 
 -- --------------------------------------------------------
 
@@ -44,11 +56,28 @@ CREATE TABLE `produtos` (
   `id_produto` int(11) NOT NULL,
   `nome` varchar(255) NOT NULL,
   `preco` decimal(10,2) NOT NULL,
-  `quant` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL,
   `marca` varchar(255) NOT NULL,
   `observacoes` varchar(255) DEFAULT NULL,
-  `id_categoria` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id_categoria` int(11) DEFAULT NULL,
+  `datacadastro` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id_produto`, `nome`, `preco`, `quantidade`, `marca`, `observacoes`, `id_categoria`, `datacadastro`) VALUES
+(1, 'Manga Palmer', '5.00', 30, 'horta do zé', 'Preço e quantidade por kg', 5, '2026-06-11'),
+(2, 'Palha de aço', '2.00', 50, 'BomBril', '', 4, '2026-06-11'),
+(3, 'Coração de galinha', '20.00', 30, 'Seara', 'Preço e quantidade por kg', 6, '2026-06-11'),
+(4, 'Filé de frago', '20.00', 80, 'Seara', 'Preço e quantidade por kg', 6, '2026-06-11'),
+(5, 'Fone sem fio', '100.00', 15, 'Realme', '', 1, '2026-06-11'),
+(6, 'Sabão em pó', '14.00', 50, 'Omo', '', 4, '2026-06-11'),
+(7, 'Desinfetante', '7.00', 50, 'UAU', '', 4, '2026-06-11'),
+(8, 'Máquina de costura', '300.00', 10, 'Singer', '', 1, '2026-06-11'),
+(9, 'Par de luva sem dedo', '7.00', 20, 'Não tem', '', 2, '2026-06-11'),
+(10, 'Par de meia', '2.50', 50, 'Não tem', '', 2, '2026-06-11');
 
 --
 -- Índices para tabelas despejadas
@@ -75,13 +104,13 @@ ALTER TABLE `produtos`
 -- AUTO_INCREMENT de tabela `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Restrições para despejos de tabelas
